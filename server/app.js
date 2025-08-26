bfed95c226e99d5812649f5b10470126e76ce2f4
HackRogue <mazumdarh549@gmail.com>
2025-08-27 02:20:35 +0530

    Added payment features but integration is left

--- server/app.js
+++ server/app.js
@@ -6,10 +6,15 @@ import {userRouter} from './routes/userRoutes.js'
 import { productRouter } from './routes/ProductRoutes.js';
 import { clubRouter } from './routes/clubRoutes.js';
 import { orderItemRouter } from './routes/orderItemRoutes.js';
+import { authRouter } from './routes/authRoutes.js';
+import cookieParser from 'cookie-parser';
+import paymentRouter from './routes/paymentRoutes.js';
+import { protect } from './controllers/authControllers.js';
 
 const prisma = new PrismaClient();
 const app = express(); 
 app.use(express.json());
+app.use(cookieParser())
 
 dotenv.config(); 
 
@@ -18,5 +23,7 @@ app.use('/users', userRouter)
 app.use('/product', productRouter)
 app.use('/club', clubRouter)
 app.use('/orderItem', orderItemRouter)
+app.use('/auth', authRouter)
+app.use('/payment', protect, paymentRouter)
 
 export {app, prisma}
